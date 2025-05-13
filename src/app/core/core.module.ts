40404b5f38e7b522c79d1ece416c629f3de806d7
Anderson1221xd <105122477+Anderson1221xd@users.noreply.github.com>
2025-05-13 16:21:24 -0500

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { SupabaseService } from '../services/supabase.service'; // Mueve tu servicio aquí
+import { SupabaseService } from '../services/supabase.service'; 
 
 @NgModule({
   providers: [
     SupabaseService,
-    // Otros servicios globales aquí
+ 
   ],
 })
 export class CoreModule {
